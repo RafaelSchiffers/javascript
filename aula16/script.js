039ef1d70bc05cf67f1a87a6fe7c2841eef7ed16
RafaelSchiffers <rsn-el@hotmail.com>
2024-07-16 16:26:29 -0300

    novo fim passo

--- aula16/script.js
+++ aula16/script.js
@@ -1,6 +1,6 @@
-function clicar() {
+/*function clicar() {
 
-
+    
     let inicio = Number(document.getElementById('iInicio').value);
     let fim = Number(document.getElementById('iFim').value);
     let passo = Number(document.getElementById('iPasso').value); 
@@ -9,7 +9,7 @@ function clicar() {
     // res.appendChild(texto)
     res.innerHTML = 'Contando: '
     
-    if (inicio <= 0 && fim <=0 && passo <=0) {
+    if (inicio == 0 || fim <=0 || passo <=0) {
         texto.innerHTML = 'Inicio e Fim Não pode ser abaixado de 0'
     }else{
         for (inicio;inicio<=fim;inicio+=passo) {
@@ -19,4 +19,40 @@ function clicar() {
     
     
      
+}
+*/
+function clicar() {
+    let inicio = document.getElementById('iInicio');
+    let fim = document.getElementById('iFim');
+    let passo = document.getElementById('iPasso');
+    var res = document.getElementById('res');
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] faltam dados!');
+    } else {
+        res.innerHTML = 'Contando: <br>';
+        let i = Number(inicio.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+
+        if(p<=0){
+            window.alert('Passo inválido! Considerando Passo 1' )
+            p = 1
+        }
+        if (i < f) {
+
+            for (i; i <= f; i += p) {
+                res.innerHTML += ` ${i} \u{1F449}`;
+            }
+        } else {
+            for (i; i => f; i -= p) {
+                res.innerHTML += ` ${i} \u{1F449}`;
+            }
+        }
+        res.innerHTML + = ` \u{1FC1}`
+
+
+
+
+    }
 }
